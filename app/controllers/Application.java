31712b7d83e3004cc4701544da2aa7c78b11e96e
Jörgen Falk <jorgen.p.falk@gmail.com>
2012-08-20 14:17:23 +0200

    Minor change to test heroku: added postgres dependency

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
   
   public static Result index() {
-    return ok(index.render("Heruko World"));
+    return ok(index.render("Welcome to Lemon drops"));
   }
   
 }
